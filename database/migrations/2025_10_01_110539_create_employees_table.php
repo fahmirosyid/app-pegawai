30facfc2faec77010bf3a6e41385aee136c4a2d8
fahmirosyid <fahmirosyid16@gmail.com>
2025-10-01 18:51:22 +0700

    CRUD tabel employee

--- database/migrations/2025_10_01_110539_create_employees_table.php
+++ database/migrations/2025_10_01_110539_create_employees_table.php
@@ -12,14 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('employees', function (Blueprint $table) {
-            $table->id(); 
-            $table->string('nama_lengkap', 100); 
-            $table->string('email', 100); 
-            $table->string('nomor_telepon', 15); 
-            $table->date('tanggal_lahir'); 
-            $table->text('alamat'); 
-            $table->date('tanggal_masuk'); 
-            $table->enum('status', ['aktif', 'nonaktif'])->default ('aktif'); 
+            $table->id();
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('employees', EmployeeController::class);
+
+Route::get('/employee', function () {
+    return "Halaman employee dengan middleware CheckEmployeeAccess";
+})->middleware('CheckEmployeeAccess');
